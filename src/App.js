5acfb0e1a837304437725d2b98422875de131076
Fern4ndox <fern4ndorecinos@gmail.com>
2020-07-15 20:41:27 -0600

    Validación de Datos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Pregunta from './components/Pregunta'
 
 
 function App() {
   return (
-   <h1>Gasto Semanal</h1>
+    <div className="container">
+      <h1>Gasto Semanal</h1>
+      <div className="contenido-principal contenido">
+      <Pregunta>
+
+
+      </Pregunta>
+      </div>
+    </div>
+   
   );
 }
 
--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,21 +1,44 @@
-import React,{Fragment} from 'react';
+import React,{Fragment, useState} from 'react';
 
 const Pregunta = () => {
-    <Fragment>
+    //State Error
+    const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError]=useState(false);
+    //Función lee presupuesto
+    const definirPresupuesto =e=>{
+        guardarCantidad(parseInt(e.target.value, 10))
+    }
+    //Submit Presupuesto
+    const agregarPresupuesto =e=>{
+        e.preventDefault();
+         //Validación
+         if(cantidad<1 || isNaN(cantidad))
+         {
+             guardarError(true)
+             return;
+         }
 
-    <h2>Coloca tu presupuesto</h2>
-    <form>
-        <input
-        type="number"
-        className="u-full-width"
-        placeholder="Coloca tu presupuesto"
-        />
-        <input
-        type="submit"
-        className="buton-primary u-full-width"
-        value="Definir presupuesto"/>
-    </form>
-    </Fragment>
+         guardarError(false)
+    }
+    return(<Fragment>
+        <h2>Coloca tu presupuesto</h2>
+
+        <form
+        onSubmit={agregarPresupuesto}
+        >    
+            <input
+            type="number"
+            className="u-full-width"
+            placeholder="Coloca tu presupuesto"
+            onChange={definirPresupuesto}
+            />
+            <input
+            type="submit"
+            className="buton-primary u-full-width"
+            value="Definir presupuesto"/>
+        </form>
+        </Fragment>
+        )
 }
  
 export default Pregunta;
