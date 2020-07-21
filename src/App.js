f385df2c16a9ce9e1ae3ef5fa6a15961078b07aa
Fernando Recinos <fern4ndorecinos@gmail.com>
2020-07-21 17:54:44 -0600

    Almacenar Gastos y Limpia Formulario

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ function App() {
   const [presupuesto, guardarPresupuesto] =useState(0); 
   const [restante, guardarRestante] = useState(0);
   const [mostrarpregunta, actualizarPregunta]= useState(true);
+  const [gastos, guardarGastos] = useState([]);
+
+  const agregarNuevoGasto = gasto => {
+    guardarGastos([
+      ...gastos,
+      gasto
+    ])
+  }
   return (
     <div className="container">
       <h1>Gasto Semanal</h1>
@@ -25,7 +33,9 @@ function App() {
           <div className="row">
           <div className="one-half column">
             1
-            <Formulario/>
+            <Formulario
+            agregarNuevoGasto={agregarNuevoGasto}
+            />
           </div>
           <div className="one-half column">
             2
